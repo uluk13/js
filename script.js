e5908dbdd0d8dbbb9f782a0a5ad45fd1125d9a38
Ulukbek <sherqzx@gmail.com>
2025-12-08 15:13:18 +0600

    init commit

--- script.js
+++ script.js
@@ -1,143 +1,77 @@
-console.log("home-work6");
+
+const array = [1, 3, 5, 2, 4]
+array.push(6)
+console.log('добавить число в конец массива:',array)
+
+array.shift()
+console.log('удалить число в начале:',array)
+
+const filter = array.filter(num => num % 2 === 0);
+console.log('только четные числа:',filter)
 
 
-var a = 'hello'
-var b = 10
-var c
+let multiplied = array.map(num => num * 5);
+console.log('каждое число умножено на 5:', multiplied);
 
-console.log(c);
-console.log(typeof a);
-console.log(typeof b);
-console.log(a - b);
-console.log(a == b);
+let average = multiplied.reduce((sum, num) => sum + num, 0) / multiplied.length;
+console.log('среднее арифметическое:', average);
+
+console.log('по возрастанию:',multiplied.sort());
 
 
-
-
-
-var sum = prompt("Введите сумму покупки:");
-sum = Number(sum);
-
-if (isNaN(sum)) {
-  console.log("Ошибка! Введите число.");
-} else {
-
-  var discount = 0;
-
-  if (sum >= 100 && sum <= 500) discount = 5;
-  else if (sum > 500 && sum <= 1000) discount = 10;
-  else if (sum > 1000) discount = 15;
-
-  var result = sum - (sum * discount / 100);
-
-  console.log("Скидка:", discount + "%");
-  console.log("Итого:", result.toFixed(2));
-}
-
-console.log("home-work7");
-
-
-const orders = [
-
-  {
-
-    id: 1,
-
-    customer: "Jhon",
-
-    items: [
-
-      { name: "Ноутбук",  price: 50000, quantity: 1 },
-
-      { name: "Мышь",     price: 1500,  quantity: 2 }
-
-    ]
-
-  },
-
-  {
-
-    id: 2,
-
-    customer: "Alan",
-
-    items: [
-
-      { name: "Телефон", price: 30000, quantity: 1 },
-
-      { name: "Чехол",   price: 1000,  quantity: 3 }
-
-    ]
-
-  },
-
-  {
-
-    id: 3,
-
-    customer: "Jane",
-
-    items: [
-
-      { name: "Клавиатура", price: 2500,  quantity: 1 },
-
-      { name: "Монитор",    price: 12000, quantity: 1 }
-
-    ]
-
-  }
-
+const celebrities = [
+  { name: "Leonardo DiCaprio", age: 49, role: "Actor" },
+  { name: "Taylor Swift", age: 34, role: "Singer" },
+  { name: "Lionel Messi", age: 37, role: "Football player" },
+  { name: "Ariana Grande", age: 31, role: "Singer" },
+  { name: "Keanu Reeves", age: 60, role: "Actor" },
+  { name: "Dwayne Johnson", age: 52, role: "Actor" },
+  { name: "Billie Eilish", age: 19, role: "Singer" },
+  { name: "Justin Bieber", age: 31, role: "Singer" },
+  { name: "Zendaya", age: 28, role: "Actress" },
+  { name: "Cristiano Ronaldo", age: 40, role: "Football player" }
 ];
 
-var total = 0
-var maxOrderSum = 0
-var maxOrder = 0
 
-for (var i = 0; i < orders.length; i++) {
-  var order = orders[i];
-  var orderSum = 0
+console.log(celebrities.map(a => a.name));
 
-  for (var j = 0; j < order.items.length; j++) {
-    var item = order.items[j];
-    orderSum += item.price * item.quantity
+console.log(celebrities.map((a) => {
+    return a.name + " is a " + a.role;
+}));
+
+
+const result = celebrities.reduce((a,b) => {
+  if (b.age < a.young.age ) {
+    a.young = b
   }
-  console.log("client", order.customer, orderSum  );
-
-  total += orderSum
-  
-  if(orderSum  > maxOrderSum) {
-    maxOrderSum = orderSum;
-    maxOrder = order
+  if (b.age > a.old.age) {
+    a.old = b
   }
-  
+  return a
+},
+  {
+  young: celebrities[0],
+  old: celebrities[0]
 }
+)
+console.log("Самый младший:",result.young);
+console.log("Самый старший",result.old);
 
-console.log("Общий доход:", total);
-console.log("самый дорогой заказ",maxOrder.customer , - maxOrderSum);
-
-
-
-console.log("home-work8");
-
-
-var a = [3, 7, 1, 9, 4];
-var b = [10, 20, 5];
-var c = [-5, -2, -10];
-
-
-function getMax(arr) {
-  var max = arr[0];
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-  }
-  return max;
-}
+console.log("Разница между старшим и младшим:",result.old.age - result.young.age);
 
 
 
-console.log("Максимальное число",getMax(a));
-console.log("Максимальное число",getMax(b)); 
-console.log("Максимальное число",getMax(c)); 
+
+console.log(celebrities.some(a => a.age < 18));
+
+console.log(celebrities.filter(a => a.age > 20));
+
+
+
+
+
+
+
+
+
 
