5a4d73fa2a8735b4c5a5df9f8ba1184be2767457
Ulukbek <sherqzx@gmail.com>
2025-12-11 14:38:24 +0600

    init commit

--- script.js
+++ script.js
@@ -1,77 +1,30 @@
+const student = { name: "Ivan", age: 21, skills: ["js", "CSS"] };
 
-const array = [1, 3, 5, 2, 4]
-array.push(6)
-console.log('добавить число в конец массива:',array)
+const { name, age, skills } = student;
+const [js, css] = skills;
+console.log(name);
+console.log(age);
+console.log(js, css);
 
-array.shift()
-console.log('удалить число в начале:',array)
+const printStudent = ({ name, age, skills }) => {
+  console.log(`Student ${name}, age ${age}`);
+};
+printStudent(student);
 
-const filter = array.filter(num => num % 2 === 0);
-console.log('только четные числа:',filter)
-
-
-let multiplied = array.map(num => num * 5);
-console.log('каждое число умножено на 5:', multiplied);
-
-let average = multiplied.reduce((sum, num) => sum + num, 0) / multiplied.length;
-console.log('среднее арифметическое:', average);
-
-console.log('по возрастанию:',multiplied.sort());
-
-
-const celebrities = [
-  { name: "Leonardo DiCaprio", age: 49, role: "Actor" },
-  { name: "Taylor Swift", age: 34, role: "Singer" },
-  { name: "Lionel Messi", age: 37, role: "Football player" },
-  { name: "Ariana Grande", age: 31, role: "Singer" },
-  { name: "Keanu Reeves", age: 60, role: "Actor" },
-  { name: "Dwayne Johnson", age: 52, role: "Actor" },
-  { name: "Billie Eilish", age: 19, role: "Singer" },
-  { name: "Justin Bieber", age: 31, role: "Singer" },
-  { name: "Zendaya", age: 28, role: "Actress" },
-  { name: "Cristiano Ronaldo", age: 40, role: "Football player" }
-];
-
-
-console.log(celebrities.map(a => a.name));
-
-console.log(celebrities.map((a) => {
-    return a.name + " is a " + a.role;
-}));
-
-
-const result = celebrities.reduce((a,b) => {
-  if (b.age < a.young.age ) {
-    a.young = b
-  }
-  if (b.age > a.old.age) {
-    a.old = b
-  }
-  return a
-},
-  {
-  young: celebrities[0],
-  old: celebrities[0]
+const sumAndMultiply = (first, ...rest) => {
+  const sum = rest.reduce((a, b) => a + b);
+  return first * sum;
 }
-)
-console.log("Самый младший:",result.young);
-console.log("Самый старший",result.old);
 
-console.log("Разница между старшим и младшим:",result.old.age - result.young.age);
+console.log(sumAndMultiply(2, 1, 3, 4)); 
 
 
+const arrayFirst = [4, 5, 6]
+const arraySecond = [1, 2]
+const arrayThird = [9, 10]
+const arrayFourth = [3]
+const arrayFiveth = [7, 8]
 
-
-console.log(celebrities.some(a => a.age < 18));
-
-console.log(celebrities.filter(a => a.age > 20));
-
-
-
-
-
-
-
-
-
+const newArr = [...arraySecond, ...arrayFourth, ...arrayFirst, ...arrayFiveth, ...arrayThird]
+console.log(newArr);
 
