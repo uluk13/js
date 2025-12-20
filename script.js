e1dd18a18f9e86e2a8bcc34a370160e73faf7b28
Ulukbek <sherqzx@gmail.com>
2025-12-20 23:46:35 +0600

    init commit

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
-const student = { name: "Ivan", age: 21, skills: ["js", "CSS"] };
+const Btn = document.querySelector("#Btn");
 
-const { name, age, skills } = student;
-const [js, css] = skills;
-console.log(name);
-console.log(age);
-console.log(js, css);
+const saved = localStorage.getItem("theme");
 
-const printStudent = ({ name, age, skills }) => {
-  console.log(`Student ${name}, age ${age}`);
-};
-printStudent(student);
-
-const sumAndMultiply = (first, ...rest) => {
-  const sum = rest.reduce((a, b) => a + b);
-  return first * sum;
+if (saved === "dark") {
+  document.body.classList.add("dark");
+} else {
+  document.body.classList.remove("dark");
 }
 
-console.log(sumAndMultiply(2, 1, 3, 4)); 
-
-
-const arrayFirst = [4, 5, 6]
-const arraySecond = [1, 2]
-const arrayThird = [9, 10]
-const arrayFourth = [3]
-const arrayFiveth = [7, 8]
-
-const newArr = [...arraySecond, ...arrayFourth, ...arrayFirst, ...arrayFiveth, ...arrayThird]
-console.log(newArr);
-
+Btn.addEventListener("click", () => {
+  if (document.body.classList.contains("dark")) {
+    document.body.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+  } else {
+    document.body.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  }
+});
